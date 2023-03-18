039526c16a7315af59cc2cfa94bb328953ab9f12
ethanblum <86435682+ethanblum@users.noreply.github.com>
2023-03-18 12:52:20 -0500

    Added More Comments

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from email.message import Message
 
 def sendEmail(name, toAddr):
     print(name, toAddr)
-    sender = "[EMAIL ADDRESS]"
-    password = "[MAIL PASSWORD]"
+    sender = "[EMAIL ADDRESS]"  # Input the users email address here
+    password = "[MAIL PASSWORD]"  # Input the users password here
 
-    subject = '[SUBJECT]'
+    subject = '[SUBJECT]'  # Input the subject title of the email
 
     # This program pulls a message out of a text document.
     # This text document is formatted to display better in an email format.
-    fileMessage = open("./venv/Message.txt").read()
+    fileMessage = open("./venv/Message.txt").read()  # make sure to update the message.txt file
 
     # Read in file message
     message = fileMessage
@@ -29,16 +29,15 @@ def sendEmail(name, toAddr):
 
 
 def getAddr():
-
     # Defined these variables in case contact.txt doesn't exist.
-    MemberName = "[DEFAULT NAME]"
-    emailAddr = "[DEFAULT EMAIL ADDRESS]"
+    MemberName = "[DEFAULT NAME]"  # Might want to define default creds
+    emailAddr = "[DEFAULT EMAIL ADDRESS]"  # Might want to define default creds
 
     # Open Contact.txt
-    file = open("./venv/Contact.txt")
+    file = open("./venv/Contact.txt")  # make sure to edit the Contact.txt file and input target email addresses
 
     # Read in file delimited by Name,Email Address
-    reader = csv.reader(file , delimiter=',')
+    reader = csv.reader(file, delimiter=',')
 
     # Input contents of file into correct var
     for row in reader:
@@ -50,17 +49,14 @@ def getAddr():
 
 
 def callMail(sender, password, toAddr, message):
-
     # Create a connection, login, and send the desired message using the format
     # provided by sendEmail()
     connection = smtplib.SMTP("smtp.gmail.com", 587)
     connection.starttls()
     connection.login(user=sender, password=password)
-    connection.sendmail(from_addr=sender, to_addrs=toAddr, msg=message,)
+    connection.sendmail(from_addr=sender, to_addrs=toAddr, msg=message, )
     connection.close()
 
 
 if __name__ == '__main__':
     getAddr()
-
-
